213c7daa1bf976abf8a890ae42dcef978a71adcd
fernando-fernandes <fernando.85@outlook.com>
2020-06-01 00:15:32 -0300

    Sass

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import Menu from './components/Menu';
-import "./App.css";
+import "./styles/css/App.css";
 
 export default function App() {
   return (
-   <BrowserRouter>
-    <section className="section">
-    <Menu />
-    <Routes />
-    </section>
-    <section className="section">
+    <BrowserRouter>
 
-    </section>
-  </BrowserRouter>
+      <section className="section section-nav">
+        <Menu />
+      </section>
+
+      <section className="section section-content">
+        <Routes />
+      </section>
+
+    </BrowserRouter>
   );
 }
