041e23f437afc8bdf9452cc91c6a1b3f5655db30
fernando-fernandes <fernando.85@outlook.com>
2020-06-01 07:53:18 -0300

    Updates

---
+++ precache-manifest.70beabc8d79308664acfe32e247abc1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06ffbee5db564f8326bb22b216fa668",
+    "url": "/E-commerce/index.html"
+  },
+  {
+    "revision": "d420aac992e0be3dd899",
+    "url": "/E-commerce/static/css/main.a3b4b9b1.chunk.css"
+  },
+  {
+    "revision": "14d456efcc8b1f15f72e",
+    "url": "/E-commerce/static/js/2.91c05a12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/E-commerce/static/js/2.91c05a12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d420aac992e0be3dd899",
+    "url": "/E-commerce/static/js/main.70b96792.chunk.js"
+  },
+  {
+    "revision": "f2e5e2409fde8f87ee98",
+    "url": "/E-commerce/static/js/runtime-main.9da57e73.js"
+  },
+  {
+    "revision": "ed5ab9405f69d01e32e83a3cc723a66d",
+    "url": "/E-commerce/static/media/Peter-Tarka.ed5ab940.png"
+  }
+]);
